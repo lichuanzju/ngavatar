3bae9eb8667d85491d9f3a98ecd581e05fed5147
lic <lichuan0502@126.com>
2015-03-11 14:40:22 +0800

    use configuration to get template/static/storage directory

--- src/scripts/cgi/configuration/__init__.py
+++ src/scripts/cgi/configuration/__init__.py
@@ -32,6 +32,18 @@ def _load_configuration(conf_filepath):
     return global_
 
 
+def get_template_filepath(template_filename):
+    """Return the absolute path to a template file."""
+    return os.path.join(SITE_CONF.get('template_path', ''),
+                        template_filename)
+
+
+def get_static_filepath(static_filename):
+    """Return the absolute path to a static file."""
+    return os.path.join(SITE_CONF.get('static_path', ''),
+                        static_filename)
+
+
 # Get absolute path for configuration file
 _conf_relative_filepath = '../../conf/ngavatar.conf'
 _current_path = os.path.dirname(os.path.realpath(__file__))
