26d79c3c9563805c8cbf5e855e4fa84496834ac3
lic <lichuan0502@126.com>
2015-03-11 10:28:25 +0800

    add HttpError and other file errors

---
+++ src/scripts/libs/ng/_http_status_code.py
@@ -0,0 +1,30 @@
+"""This module defines http status codes and their description."""
+
+_http_status_code = {
+    200: 'OK',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+}
+
+
+def http_code_description(http_code):
+    """return description of a specified http status code"""
+    if http_code in _http_status_code:
+        return _http_status_code[http_code]
+    else:
+        return 'Unknown Status Code'
+
+
+def test():
+    print http_code_description(404)
+    print http_code_description(100)
+
+
+if __name__ == '__main__':
+    test()
--- src/scripts/libs/ng/excepts.py
+++ src/scripts/libs/ng/excepts.py
@@ -1,16 +1,49 @@
-"""This module defines exceptions that may be raised in this package"""
+"""This module defines exceptions that may be raised in this package."""
 
 
 class NGError(Exception):
-    """Base class for exceptions in this package"""
+    """Base class for exceptions in this package."""
     pass
 
 
-class FileReadError(NGError):
-    """Exception that is raised when failed to read a file"""
+class HttpError(NGError):
+    """Error that is raised when an http error code is required to send."""
+
+    def __init__(self, error_code):
+        "Create an http error with http error code."
+        self.error_code = error_code
+
+    def http_status(self):
+        """Return http status description of this error"""
+        import _http_status_code
+        return "%d %s" % (self.error_code, \
+            _http_status_code.http_code_description(self.error_code))
+
+    def __str__(self):
+        "Return description of this error."
+        import _http_status_code
+        return _http_status_code.http_code_description(self.error_code)
+
+
+class FileOpenError(HttpError):
+    """Error that is raised when failed to open a file."""
+
+    def __init__(self, filepath):
+        """Create file open error with path to the file."""
+        HttpError.__init__(self, 404)
+        self.filepath = filepath
+
+    def __str__(self):
+        """Return description of this error."""
+        return 'Failed to open file "%s"' % self.filepath
+
+
+class FileReadError(HttpError):
+    """Error that is raised when failed to read a file."""
 
     def __init__(self, filepath):
         """Create file read error with path to the file."""
+        HttpError.__init__(self, 403)
         self.filepath = filepath
 
     def __str__(self):
@@ -18,11 +51,25 @@ class FileReadError(NGError):
         return 'Failed to read file "%s"' % self.filepath
 
 
-class TemplateFormatError(NGError):
-    """Exception that is raised when format of a template is illegal."""
+class FileWriteError(HttpError):
+    """Error that is raised when failed to write a file."""
+
+    def __init__(self, filepath):
+        """Create file write error with path to the file."""
+        HttpError.__init__(self, 500)
+        self.filepath = filepath
+
+    def __str__(self):
+        """Return description of this error."""
+        return 'Failed to write file "%s"' % self.filepath
+
+
+class TemplateFormatError(HttpError):
+    """Error that is raised when format of a template is illegal."""
 
     def __init__(self, template_filepath, reason=None):
         """Create template format error with path to the file."""
+        HttpError.__init__(self, 500)
         self.template_filepath = template_filepath
         self.reason = reason
 
@@ -34,14 +81,28 @@ class TemplateFormatError(NGError):
 
 def test_Exceptions():
     try:
-        raise FileReadError('/tmp/view')
+        raise FileOpenError('/tmp/view')
+    except FileOpenError as e:
+        print e
+        print e.http_status()
+
+    try:
+        raise FileReadError('/tmp/template.html')
     except FileReadError as e:
         print e
+        print e.http_status()
+
+    try:
+        raise FileWriteError('/tmp/template.html')
+    except FileWriteError as e:
+        print e
+        print e.http_status()
 
     try:
         raise TemplateFormatError('/tmp/template.html')
     except TemplateFormatError as e:
         print e
+        print e.http_status()
 
 
 if __name__ == '__main__':
