8eee93552aad91b281514843f9f2d756496af794
lic <lichuan0502@126.com>
2015-03-13 23:17:36 +0800

    add unique generate function to str_generator module

--- src/scripts/libs/ng/str_generator.py
+++ src/scripts/libs/ng/str_generator.py
@@ -4,6 +4,7 @@
 import string
 import random
 import hashlib
+import uuid
 
 
 def random_string(size=6, chars=string.ascii_letters + string.digits):
@@ -19,3 +20,9 @@ def sha1_hexdigest(input_string, size=20):
     sha1 = hashlib.sha1()
     sha1.update(input_string)
     return sha1.hexdigest()[0:size]
+
+
+def unique_id(size=40):
+    """Generate unique ID. size specifies the number of characters in the
+    ID, which is 40 by default."""
+    return sha1_hexdigest(str(uuid.uuid1()), size).upper()
