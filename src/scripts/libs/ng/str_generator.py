45d82ae8d145cbe4cdcc67dd5eca95e5e4973187
lic <lichuan0502@126.com>
2015-03-13 23:21:09 +0800

    describe representation and maximum length of generated strings

--- src/scripts/libs/ng/str_generator.py
+++ src/scripts/libs/ng/str_generator.py
@@ -15,14 +15,15 @@ def random_string(size=6, chars=string.ascii_letters + string.digits):
 
 
 def sha1_hexdigest(input_string, size=20):
-    """Generate hexdigest for the input string. size specifies the number
-    of hex digits in the digest, which is 20 by default."""
+    """Generate hexdigest(lower case) for the input string.
+    size specifies the number of hex digits in the digest,
+    which is 40 in maximum."""
     sha1 = hashlib.sha1()
     sha1.update(input_string)
     return sha1.hexdigest()[0:size]
 
 
 def unique_id(size=40):
-    """Generate unique ID. size specifies the number of characters in the
-    ID, which is 40 by default."""
+    """Generate unique ID(as upper case hex string). size specifies
+     the number of characters in the ID, which is 40 in maximum."""
     return sha1_hexdigest(str(uuid.uuid1()), size).upper()
