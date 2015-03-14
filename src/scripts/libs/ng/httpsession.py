449c307c78899938a82a6d72fd316e8250681c93
lic <lichuan0502@126.com>
2015-03-14 11:23:49 +0800

    add DatabaseSession class

--- src/scripts/libs/ng/httpsession.py
+++ src/scripts/libs/ng/httpsession.py
@@ -2,7 +2,10 @@
 
 
 import abc
+import datetime
 from database import MySQLDatabase
+from models import Session
+import str_generator
 
 
 class HttpSession(object):
@@ -53,3 +56,64 @@ class HttpSession(object):
     def invalidate(self):
         """Remove this session."""
         pass
+
+
+class DatabaseSession(HttpSession):
+    """Http session implemented with database."""
+
+    def __init__(self, db, model):
+        """Create session object."""
+        self.db = db
+        self.model = model
+
+    @classmethod
+    def create_session(cls, db, data, client_ip, effective_hours):
+        """Create a new session in database."""
+        session_key = str_generator.unique_id(40)
+
+        session_model = Session.create_session(
+            db,
+            session_key,
+            data,
+            client_ip,
+            effective_hours
+        )
+
+        return DatabaseSession(db, session_model)
+
+    @classmethod
+    def load_session(cls, db, session_key):
+        """Load session from database with session_key."""
+        session_model = Session.load_from_database(db,
+                                                   session_key=session_key)
+
+        if session_model is None:
+            return None
+        else:
+            return DatabaseSession(db, session_model)
+
+    def get_session_key(self):
+        """Return the key of this session."""
+        return self.model['session_key']
+
+    def get_attribute(self, attribute_name, default=None):
+        """Get attribute stored in this session with specified name."""
+        return self.model.get_data_attribute(attribute_name, default)
+
+    def set_attribute(self, attribute_name, attribute_value):
+        """Set the value of attribute with specified name."""
+        self.model.set_data_attribute(attribute_name,
+                                      attribute_value)
+        self.model.store_session_data(self.db)
+
+    def expired(self):
+        """Return whether this session has expired."""
+        return self.model.session_expired()
+
+    def renew(self, effective_hours):
+        """Renew the expiring time of this session."""
+        return self.model.renew_session(self.db, effective_hours)
+
+    def invalidate(self):
+        """Remove this session."""
+        self.model.delete_from_database(self.db)
