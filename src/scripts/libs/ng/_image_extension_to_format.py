3d06288280a92c7cd830c8bdb9d642e9d4ecfce2
lic <lichuan0502@126.com>
2015-03-14 23:07:01 +0800

    use function attribute to store image format mapping in _image_extension_to_format module

--- src/scripts/libs/ng/_image_extension_to_format.py
+++ src/scripts/libs/ng/_image_extension_to_format.py
@@ -1,22 +1,21 @@
 """This module defines the utility functions for converting image
 extension to format."""
 
-_image_formats = {
-    '.bmp': 'bmp',
-    '.fax': 'fax',
-    '.gif': 'gif',
-    '.ico': 'x-icon',
-    '.jpe': 'jpeg',
-    '.jpeg': 'jpeg',
-    '.tif': 'tiff',
-    '.tiff': 'tiff',
-    '.png': 'png',
-}
-
 
 def image_format_from_extension(extension):
     """Get image format from its extension."""
-    if extension in _image_formats:
-        return _image_formats[extension]
-    else:
-        return 'jpeg'
+    if not hasattr(image_format_from_extension, '_image_formats'):
+        image_format_from_extension._image_formats = {
+            '.bmp': 'bmp',
+            '.fax': 'fax',
+            '.gif': 'gif',
+            '.ico': 'x-icon',
+            '.jpe': 'jpeg',
+            '.jpeg': 'jpeg',
+            '.tif': 'tiff',
+            '.tiff': 'tiff',
+            '.png': 'png',
+        }
+
+    return image_format_from_extension._image_formats.\
+        get(extension, 'jpeg')
