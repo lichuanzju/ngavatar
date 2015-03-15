60d285e2653f76237075947420fa4f248c9ad72a
lic <lichuan0502@126.com>
2015-03-15 16:21:10 +0800

    use template to generate index page

--- src/scripts/cgi/handlers/_index.py
+++ src/scripts/cgi/handlers/_index.py
@@ -1,7 +1,7 @@
 """This module defines handler that handles the /index request."""
 
 
-from ng.views import StaticView
+from ng.views import TemplateView
 from ng.http import HttpResponse
 from ng import httpfilters
 import config
@@ -10,5 +10,14 @@ import config
 @httpfilters.method_checker('GET')
 def handler(request, conf):
     """The handler function."""
-    index_view = StaticView(config.static_filepath('index.html'))
+    template_args = dict(
+        page_name='Welcome',
+        site_name=conf.get('name', '')
+    )
+
+    index_view = TemplateView(
+        config.template_filepath('index.html'),
+        template_args
+    )
+
     return HttpResponse(index_view)
