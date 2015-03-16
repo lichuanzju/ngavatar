06b9fc3487821050374eeb95beec538af1fbf12d
lic <lichuan0502@126.com>
2015-03-16 20:33:34 +0800

    check session while handling index requests

--- src/scripts/cgi/handlers/_index.py
+++ src/scripts/cgi/handlers/_index.py
@@ -1,18 +1,20 @@
 """This module defines handler that handles the /index request."""
 
 
-from ng.views import TemplateView
-from ng.http import HttpResponse
 from ng import httpfilters
+from ng.database import MySQLDatabase
+from ng.http import HttpResponse
+from ng.models import Account
+from ng.views import TemplateView
 import config
+import _session_check
 
 
-@httpfilters.allow_methods('GET')
-def handler(request, conf):
-    """The handler function."""
+def index_response(conf, account=None):
+    """Generate response that shows the page with no user signed in."""
     template_args = dict(
-        page_name='Welcome',
-        site_name=conf.get('name', '')
+        site_name=conf.get('name', ''),
+        account=account
     )
 
     index_view = TemplateView(
@@ -21,3 +23,29 @@ def handler(request, conf):
     )
 
     return HttpResponse(index_view)
+
+
+@httpfilters.allow_methods('GET')
+def handler(request, conf):
+    """The handler function."""
+    with MySQLDatabase(conf.get('database_connection')) as db:
+        # Get session from database
+        session = _session_check.get_session(request, db)
+
+        # Check session, redirect request to /usermain if valid
+        if session is None:
+            return index_response(conf)
+
+        # Check session expiring time
+        if session.expired():
+            session.delete_from_database(db)
+            return index_response(conf)
+
+        # Check session data
+        uid = session.get_attribute('UID')
+        if uid is None:
+            return index_response(conf)
+
+        # Load account
+        account = Account.load_from_database(db, uid=uid)
+        return index_response(conf, account)
