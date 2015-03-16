445d6ebad17bb60a33a6dda8c7ba63fb3ae89a33
lic <lichuan0502@126.com>
2015-03-16 20:08:30 +0800

    check session while handling sign in request

--- src/scripts/cgi/handlers/_signin.py
+++ src/scripts/cgi/handlers/_signin.py
@@ -1,15 +1,28 @@
 """This module defines the handler that handlers /signin request."""
 
 
-from ng.views import TemplateView
-from ng.http import HttpResponse
 from ng import httpfilters
+from ng.database import MySQLDatabase
+from ng.http import HttpResponse, HttpRedirectResponse
+from ng.views import TemplateView
 import config
+import _session_check
 
 
 @httpfilters.allow_methods('GET')
 def handler(request, conf):
     """The handler function."""
+    with MySQLDatabase(conf.get('database_connection')) as db:
+        # Get session from database
+        session = _session_check.get_session(request, db)
+
+        # Check session, redirect request to /usermain if valid
+        if session is not None:
+            if session.expired():
+                session.delete_from_database(db)
+            else:
+                return HttpRedirectResponse('/usermain')
+
     username = request.field_storage.getvalue('username', '')
 
     template_args = dict(
