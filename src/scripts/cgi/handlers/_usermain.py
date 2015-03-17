81886c7d10aa4894c9046b807e8a8ff1314b0a2b
lic <lichuan0502@126.com>
2015-03-17 09:56:59 +0800

    use _accounthelper module to check for account signed in

--- src/scripts/cgi/handlers/_usermain.py
+++ src/scripts/cgi/handlers/_usermain.py
@@ -4,10 +4,10 @@
 from ng import httpfilters
 from ng.database import MySQLDatabase
 from ng.models import Account, Email, Avatar
-from ng.http import HttpResponse
+from ng.http import HttpCookie, HttpResponse, HttpRedirectResponse
 from ng.views import TemplateView
 import config
-import _sessionhelper
+import _accounthelper
 
 
 def usermain_response(db, account, conf):
@@ -36,39 +36,10 @@ def usermain_response(db, account, conf):
 def handler(request, conf):
     """The handler function."""
     with MySQLDatabase(conf.get('database_connection')) as db:
-        # Get session from database
-        session = _sessionhelper.get_session(request, db)
+        # Try to get the signed in account
+        account_or_response = _accounthelper.check_signed_in(request, db)
+        if isinstance(account_or_response, HttpResponse):
+            return account_or_response
 
-        # Redirect request to sign in page if session doesn't exist
-        if session is None:
-            return HttpRedirectResponse('/signin')
-
-        # Redirect request to sign in page and expire cookie if session
-        # is invalid
-        uid = session.get_attribute('UID')
-        if session.expired() or not uid:
-            session.invalidate()
-            cookie = _sessionhelper.expire_cookie_for_session(
-                session,
-                '/',
-                request.server_name
-            )
-            response = HttpRedirectResponse('/signin')
-            response.set_cookie(cookie)
-            return response
-
-        # Redirect request to sign in page and expire cookie if uid is
-        # invalid
-        account = Account.load_from_database(db, uid=uid)
-        if account is None:
-            session.invalidate()
-            cookie = _sessionhelper.expire_cookie_for_session(
-                session,
-                '/',
-                request.server_name
-            )
-            response = HttpRedirectResponse('/signin')
-            response.set_cookie(cookie)
-            return response
-
+        account = account_or_response
         return usermain_response(db, account, conf)
